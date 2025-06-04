4d06df59831c829fa4275ad287cc5cb68355a663
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-06-04 12:45:37 -0400

    basic game loop

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ def main():
     print("Screen width: " + str(SCREEN_WIDTH))
     print("Screen height: " + str(SCREEN_HEIGHT))
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
