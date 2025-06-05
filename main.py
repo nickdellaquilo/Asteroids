8854f39931d2df04b26666bab27a69ccfb640336
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-06-05 14:04:32 -0400

    player class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -8,12 +9,18 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(144)/1000
 
 if __name__ == "__main__":
     main()
