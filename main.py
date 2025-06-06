ee5b3aee6490f18ea097f692edd43f908734464f
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-06-06 15:25:27 -0400

    add asteroidfield.py, fix asteroid to be class

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -14,8 +16,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable  = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    field  = AsteroidField()
 
     while(True):
         for event in pygame.event.get():
